535fdbb9d903890741669eaeec028c14a671704a
Enrique-LP <jesus902009@live.com>
2024-11-28 13:21:57 -0600

    Se agrego la clase  MainApp como punto de entrada de la aplicación

---
+++ src/MainApp.java
@@ -0,0 +1,17 @@
+
+/**
+ * The {@code MainApp} class serves as the entry point of the application.
+ * It initializes and runs the main program interface for interacting with the Caesar Cipher, Brute Force,
+ * and FileManager functionalities.
+ */
+public class MainApp {
+    /**
+     * The main method that starts the execution of the application.
+     *
+     * @param args command-line arguments (not used)
+     */
+    public static void main(String[] args) {
+        Main mainProgram = new Main();
+        mainProgram.run();
+    }
+}
